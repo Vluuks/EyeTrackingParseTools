4f20110e3c004174bae7209621acd9a4711027fb
Renske <vluuks@gmail.com>
2019-03-26 13:12:12 +0100

    pandas is a thing

--- Python/eyetracking_parser.py
+++ Python/eyetracking_parser.py
@@ -1,4 +1,6 @@
 import csv
+import pandas as pd
+import numpy as np
 
 def parse_file(filename):
 
@@ -8,12 +10,7 @@ def parse_file(filename):
     with open('../Data/test.tsv') as f:
         reader = csv.DictReader(f, dialect='excel-tab')
     
-        # iterate over rows
-
-        # temporary metric so long as we have no aoi or line data
-        # this is inaccurate for now because it assumes every measurement represents a new line
-        # which is of course not true
-        linenumber = 10
+        # AOI[Rectangle 14]Hit is the first line
 
         for row in reader:
             # print(row)
@@ -24,7 +21,7 @@ def parse_file(filename):
                 x_coor = row['FixationPointX (MCSpx)']
                 y_coor = row['FixationPointY (MCSpx)']
 
-                aoi = determine_aoi(x_coor, y_coor)
+                aoi = 1
                 linenumber += 1
 
                 parsed_data.append([row['RecordingTimestamp'], row['GazeEventDuration'], x_coor, y_coor, str(aoi), str(linenumber // 10) ])
@@ -32,11 +29,6 @@ def parse_file(filename):
     
     # write new data to file
     write_file(parsed_data)
-
-
-# determines to which aoi point belongs based on 
-def determine_aoi(x, y):
-    return 1
 
 def write_file(output):
     with open('testing_things.csv', 'w') as f:
@@ -49,5 +41,28 @@ def write_file(output):
     f.close()
 
 
+def parse_file_pandas():
+
+    # grab file from csv
+    df = pd.read_csv('test_cleaned_linenumbers.csv')
+    # print(df)
+    
+
+    # rename columns to more sensible names
+    for column in df:
+        if "Rectangle" in df[column]:
+            print(df[column])
+
+
+# USE EVAL?
+    # df.rename({'$a':'a', '$b':'b', '$c':'c', '$d':'d', '$e':'e'}, axis='columns')
+
+    # keep list of said names for the next call of melt
+
+    # merge line number columns into one
+    # df.melt(id_vars=['country','year','perc'])
+
+    
 # main
-parse_file("nothing for now")
+# parse_file("nothing for now")
+parse_file_pandas()
